db8d7042a6681a669704b08ea618bdc414223615
Nick Terekhin <nick.terekhin@gmail.com>
2017-12-24 02:41:03 +0200

    prevent access for non admin user to certain tools

--- functions.php
+++ functions.php
@@ -5,7 +5,4 @@ define("CHILD_THEME_PATH",get_stylesheet_directory());
 define("CHILD_THEME_MAIN_STYLE",get_stylesheet_uri());
 define("CHILD_THEME_UPLOAD_URI",wp_upload_dir()['baseurl']);
 
-function newser_enqueue_child_theme_styles() {
-	wp_enqueue_style( 'newser-child-theme-style', get_stylesheet_uri(), array(), null );
-}
-add_action( 'wp_enqueue_scripts', 'newser_enqueue_child_theme_styles', 30 );
+include('inc/config.php');
