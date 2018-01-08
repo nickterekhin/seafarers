8f64eff7cc447e07a410447cb74dcb348a002e54
Nick Terekhin <nick.terekhin@gmail.com>
2018-01-08 17:49:02 +0200

    header styling

--- functions.php
+++ functions.php
@@ -6,3 +6,11 @@ define("CHILD_THEME_MAIN_STYLE",get_stylesheet_uri());
 define("CHILD_THEME_UPLOAD_URI",wp_upload_dir()['baseurl']);
 
 include('inc/config.php');
+
+add_action('wp_enqueue_scripts', 'main_style_setup',20);
+function main_style_setup()
+{
+    wp_register_style( 'custom-css', CHILD_THEME_PATH_URI.'/content/css/custom.css?1');
+    wp_enqueue_style( 'custom-css' );
+
+}
