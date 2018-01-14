f65f058fabe42d3ea0d307da4bc001c9bb47f295
NickTer <nick.terekhin@gmail.com>
2018-01-14 22:49:43 +0200

    added category's image as the default for news layouts

--- framework/TD_Framework.php
+++ framework/TD_Framework.php
@@ -34,7 +34,7 @@ class TD_Framework
   js = d.createElement(s); js.id = id;
   js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11';
   fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>'}
+}(document, 'script', 'facebook-jssdk'));</script>
 
         <?php
     }
