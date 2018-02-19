485bef8ab5811608ef803638f327f0e91b17a5cb
Nick Terekhin <nick.terekhin@gmail.com>
2018-02-19 04:53:13 +0200

    reworked query that drives the main slider

--- src/short_codes/view/templates/slider_parts/title.php
+++ src/short_codes/view/templates/slider_parts/title.php
@@ -4,6 +4,6 @@ $title_tag = isset($title_tag) ? $title_tag : 'h3';
 
 <<?php echo esc_attr($title_tag);?> itemprop="name" class="entry-title qode-post-title">
 <a itemprop="url" href="<?php echo get_the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-    <?php the_title(); the_ID(); ?>
+    <?php the_title(); ?>
 </a>
 </<?php echo esc_attr($title_tag);?>>
