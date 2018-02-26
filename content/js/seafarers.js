10f882cfc36348621fcb85a9447c66c00c392f52
Nick Terekhin <nick.terekhin@gmail.com>
2018-02-26 15:20:12 +0200

    styling category page

--- content/js/seafarers.js
+++ content/js/seafarers.js
@@ -1,11 +1,30 @@
 
+var dpOptions = {
+    changeMonth: true,
+    changeYear: true,
+    closeText: 'Закрыть',
+    prevText: 'Пред',
+    nextText: 'След',
+    currentText:'Сегодня',
+    monthNames:['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+    monthNamesShort: ['Янв','Фев','Мрт','Апр','Май','Июн','Июл','Авг','Сен','Окт','Нбр','Дек'],
+    dayNames: ['Понедельник','Вторник','Среда','Чеверг','Пятница','Суббота','Воскресенье'],
+    dayNamesShort: ['Пн','Вт','Ср','Чт','Пт','Сб','Вс'],
+    dayNamesMin: ['Пн','Вт','Ср','Чт','Пт','Сб','Вс'],
+    dateFormat: 'dd-mm-yy'
+};
+
+
 (function($,obj){
+
+
 
     obj.news_date_filter = function(options)
     {
         var opt = $.extend({
             container_id:"terekhindev-news-date-search"
         },options);
+        opt = $.extend(opt,dpOptions);
         var dp = $("#"+opt.container_id).datepicker(opt);
         console.log(dp);
     }
