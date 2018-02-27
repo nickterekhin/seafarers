879363bfac238e9d5b86ded67c21b06c616e559c
Nick Terekhin <nick.terekhin@gmail.com>
2018-02-27 14:20:47 +0200

    styling category page

--- src/framework/TD_Framework.php
+++ src/framework/TD_Framework.php
@@ -62,6 +62,14 @@ class TD_Framework extends TD_Framework_Base
     {
 
     }
+    function format_post_date_masonry($date,$format,$post)
+    {
+        if(is_category())
+        {
+            return '12';
+        }
+        return $date;
+    }
 }
 
 function init_framework()
