6aaf05df69aa51da5013419cf62608ea6f032c3e
Nick Terekhin <nick.terekhin@gmail.com>
2018-02-27 14:21:59 +0200

    styling category page

--- inc/news_config.php
+++ inc/news_config.php
@@ -3,4 +3,4 @@
 global $terekhin_framework;
 
 add_filter('qode_themename_filter_blog_template_read_more_button',array($terekhin_framework,'translate_read_more'),10,1);
-add_filter('get_the_time',array($terekhin_framework,'format_post_date_masonry'));
+add_filter('get_the_time',array($terekhin_framework,'format_post_date_masonry'),10,3);
