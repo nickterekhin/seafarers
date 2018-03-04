a9e657b2818529e5931bfcfc9ab806481a6b42c7
Nick Terekhin <nick.terekhin@gmail.com>
2018-03-04 19:43:51 +0200

    added popular section to the section template

--- src/view/parts/date.php
+++ src/view/parts/date.php
@@ -1,10 +1,11 @@
 <?php
+var_dump($post->ID);
 $month = get_the_time('m',$post->ID);
 $year = get_the_time('Y',$post->ID);
 $title = get_the_title($post->ID);
 
 $date_format = isset($date_format) && $date_format !== '' ? $date_format : 'published';
-$difference = human_time_diff( get_the_time('U'), current_time('timestamp') ) . esc_html__(' ago','qode-news');
+$difference = human_time_diff( get_the_time('U',$post->ID), current_time('timestamp') ) . esc_html__(' ago','qode-news');
 
 $display_date = isset($display_date) && $display_date !== '' ? $display_date : 'yes';
 
