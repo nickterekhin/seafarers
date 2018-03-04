23178afb29fc20106b6b0006c87d12859c779905
Nick Terekhin <nick.terekhin@gmail.com>
2018-03-04 20:32:59 +0200

    added popular section to the section template

--- templates/td_sidebar-news_parts.php
+++ templates/td_sidebar-news_parts.php
@@ -6,11 +6,8 @@ $obj=$wp_query->get_queried_object();
 ?>
 <div class="column_inner">
     <?php $terekhin_framework->show_hot_news_in_section($obj);?>
-    <?php $terekhin_framework->showSeparator("События в разделе",$obj,'separator_align_left');?>
-    <?php $terekhin_framework->show_post_in_section($obj,null,'events');?>
-    <?php $terekhin_framework->showSeparator("Мнения в разделе",$obj,'separator_align_left');?>
-    <?php $terekhin_framework->show_post_in_section($obj,null,'opinions');?>
-    <?php $terekhin_framework->showSeparator("Видеo в разделе",$obj,'separator_align_left');?>
-    <?php $terekhin_framework->show_post_in_section($obj,null,'videos');?>
+    <?php $terekhin_framework->show_post_in_section($obj,"События в разделе",'events');?>
+    <?php $terekhin_framework->show_post_in_section($obj,"Мнения в разделе",'opinions');?>
+    <?php $terekhin_framework->show_post_in_section($obj,"Видеo в разделе",'videos');?>
 
 </div>
