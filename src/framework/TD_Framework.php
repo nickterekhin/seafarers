23177f6e0930f5a91ad5ad7a973a337c2250e3bf
Nick Terekhin <nick.terekhin@gmail.com>
2018-03-06 22:12:55 +0200

    styled footer

--- src/framework/TD_Framework.php
+++ src/framework/TD_Framework.php
@@ -179,12 +179,12 @@ class TD_Framework extends TD_Framework_Base
     function set_post_image_css_value_from_category($output)
     {
         global $post;
-        var_dump(is_admin());
-        if(!is_admin())
+
+        if(!is_front_page())
         {
-            var_dump($output);
+
             if(preg_match('/url(\'(.*?vc_gitem_image_\.png\')/',$output,$m)==1) {
-                var_dump($m);
+
                 $cat = wp_get_post_terms($post->ID, 'category');
                 $img = get_field('header_image', 'category_' . $cat[0]->term_id);
                 if ($img) {
