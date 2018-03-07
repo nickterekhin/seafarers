c7bc9bae6888a19e5a932b982c13c46c2cc87e9b
Nick Terekhin <nick.terekhin@gmail.com>
2018-03-07 03:39:37 +0200

    styled footer

--- src/framework/TD_Framework.php
+++ src/framework/TD_Framework.php
@@ -214,15 +214,15 @@ class TD_Framework extends TD_Framework_Base
             'type' => 'zero',
             'box' => 'no',
             'position' => 'center',
-            'font-color'=>'#fff',
+            'font_color'=>'#fff',
             'text_font-weight' => '500',
             'text_transform' => 'uppercase',
             'separator' => 'yes',
             'digit' => $this->get_news_quantity_in_section($section_slug),
             'font_size' => 28,
             'text' => $text,
-            'text-size'=>14,
-            'text-color'=>'#fff',
+            'text_size'=>14,
+            'text_color'=>'#fff',
             'separator_color'=>'#fff'
         );
 
