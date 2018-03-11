963bad257b09bf576ba4b9682fa668e2348ea943
Nick Terekhin <nick.terekhin@gmail.com>
2018-03-11 22:01:53 +0200

    styling section news

--- src/view/parts/date.php
+++ src/view/parts/date.php
@@ -9,20 +9,25 @@ $difference = human_time_diff( get_the_time('U',$post->ID), current_time('timest
 $display_date = isset($display_date) && $display_date !== '' ? $display_date : 'yes';
 
 if ($display_date == 'yes'){ ?>
-	<div itemprop="dateCreated" class="qode-post-info-date entry-date published updated">
-	    <?php if(empty($title) && qode_blog_item_has_link()) { ?>
-	        <a itemprop="url" href="<?php the_permalink($post->ID) ?>">
-	    <?php } else { ?>
-	        <a itemprop="url" href="<?php echo get_month_link($year, $month); ?>">
-	    <?php } ?>
-        <i class="dripicons-alarm"></i>
-    	<?php if ($date_format == 'published') {
+	<div itemprop="dateCreated" class="qode-post-info-date entry-date published updated td-additional-post-info-layouts">
 
-    		echo get_the_time(get_option('date_format'),$post->ID);
-    	} else {
-    		echo esc_html($difference);
-    	} ?>
-        </a>
+				<ul>
+					<li><?php if(empty($title) && qode_blog_item_has_link()) { ?>
+						<a itemprop="url" href="<?php the_permalink($post->ID) ?>">
+							<?php } else { ?>
+							<a itemprop="url" href="<?php echo get_month_link($year, $month); ?>">
+								<?php } ?>
+								<i class="dripicons-alarm"></i>
+								<?php if ($date_format == 'published') {
+
+									echo get_the_time(get_option('date_format'),$post->ID);
+								} else {
+									echo esc_html($difference);
+								} ?>
+							</a></li>
+					<li><i class="fa fa-eye"></i>44567</li>
+					<li><i class="fa fa-comments"></i>456879</li>
+				</ul>
 	    <meta itemprop="interactionCount" content="UserComments: <?php echo get_comments_number(qode_get_page_id()); ?>"/>
 	</div>
 <?php } ?>
