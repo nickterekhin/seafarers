165291e29ae43496be41acf17029e24480ddbdba
Nick Terekhin <nick.terekhin@gmail.com>
2018-03-12 01:36:39 +0200

    styling section news

--- templates/additional_news.php
+++ templates/additional_news.php
@@ -9,7 +9,10 @@ $terms = get_the_terms($obj->ID,'category');
 <?php foreach($terms as $term){ ?>
 <?php $terekhin_framework->show_news_in_single_post($term->slug,'Читать так же в '.$term->name,'latest',6,'layout1-news');?>
     <?php $terekhin_framework->showSeparator('20','20',null,'transparent');?>
-    <?php $terekhin_framework->showQ2Button($term->slug)?>
+    <div style="text-align:right">
+        <?php $terekhin_framework->showQ2Button($term->slug)?>
+    </div>
+
     <?php $terekhin_framework->showSeparator('20','20',null,'transparent');?>
 
 <?php } ?>
