5ca381b39d6ed8b89c841b4a321e516181dda8e7
Nick Terekhin <nick.terekhin@gmail.com>
2018-03-19 03:06:39 +0200

    styling section news

--- src/view/layout1-news.php
+++ src/view/layout1-news.php
@@ -4,7 +4,13 @@ global $post;
 if(count($posts)>0):
     ?>
 <div class="qode-news-holder qode-layout1 qode-news-columns-3 qode-nl-normal-space qode-center-alignment"><?php if(isset($layout_title)):?> <div class="qode-news-list-title-holder"><h3 class="qode-news-layout-title"><?php echo $layout_title;?></h3></div><?php endif;?><div class="qode-news-list-inner-holder" data-number-of-items="3"><?php foreach($posts as $p): ?><div class="qode-news-item qode-layout1-item"><div class="qode-news-item-image-holder">                <div class="qode-post-image"><?php
-                    $post = !$post?$p:$post;
+                    if(!is_single()) {
+                        $post = !$post ? $p : $post;
+                    }else
+                    {
+                        $post = $p;
+                    }
+
                     $single['class']=$class;
                     echo $class->View('parts/image',$single);
                     ?>
