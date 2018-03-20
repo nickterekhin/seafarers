2abf05c328a0a66a891c33b4cdc4d577caafd26a
Nick Terekhin <nick.terekhin@gmail.com>
2018-03-20 16:17:24 +0200

    styling section news

--- src/td_news_short_codes/src/impl/TD_News_Layout1.php
+++ src/td_news_short_codes/src/impl/TD_News_Layout1.php
@@ -128,9 +128,9 @@ class TD_News_Layout1 extends TD_News_Base
 
             $this->short_code_params['posts_per_page']=9;
             $this->short_code_params['order']='DESC';
-            //add_filter('posts_where',array($this,'custom_where_filter_posts'));
+            add_filter('posts_where',array($this,'custom_where_filter_posts'));
             $query = $this->theme_tools->get_post_query($this->short_code_params);
-            //remove_filter('posts_where',array($this,'custom_where_filter_posts'));
+            remove_filter('posts_where',array($this,'custom_where_filter_posts'));
             $this->short_code_params['section_1_columns_qty']='1';
             if($query->post_count>1)
                 $this->short_code_params['section_1_columns_qty']='2';
