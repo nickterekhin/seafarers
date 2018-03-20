f237d49cd78697c31b298da3478eb0b7e6df0a2c
Nick Terekhin <nick.terekhin@gmail.com>
2018-03-20 15:57:58 +0200

    styling section news

--- src/view/l1/news_item.php
+++ src/view/l1/news_item.php
@@ -9,7 +9,7 @@
     <div class="td-news-content">
         <?php echo $obj->View('parts/category',$params);?>
         <?php echo $obj->View('parts/title',$params);?>
-        <?php echo $obj->View('parts/date',$params);?>
         <?php echo $obj->View('parts/excerpt',$params);?>
+        <?php echo $obj->View('parts/date',$params);?>
     </div>
 </div>
