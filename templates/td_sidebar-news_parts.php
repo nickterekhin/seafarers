21d48df629b8829f75c76db00015d602ae0e5d93
Nick Terekhin <nick.terekhin@gmail.com>
2018-03-25 23:41:32 +0300

    created complex short code for the main page

--- templates/td_sidebar-news_parts.php
+++ templates/td_sidebar-news_parts.php
@@ -84,12 +84,12 @@ if($obj) {
 }
 ?>
 <div class="column_inner">
-    <?php $terekhin_framework->show_news_in_section($obj,($sub_title?$sub_title.' - Популярное':'Популярные Новости'),array('sort'=>'popular','news_period'=>'2','extra_class_name'=>'td-news-list'));?>
-    <?php $terekhin_framework->show_news_in_section_by_category($obj,($sub_title?$sub_title.' - ':'')."События",'events',array('display_read_more_button'=>'yes','extra_class_name'=>'td-news-list'));?>
-    <?php $terekhin_framework->show_news_in_section_by_category($obj, ($sub_title?$sub_title.' - ':'')."Мнения", 'opinions',array('display_read_more_button'=>'yes','extra_class_name'=>'td-news-list'));
+    <?php $terekhin_framework->show_news_in_section($obj,($sub_title?$sub_title.' - Популярное':'Популярные Новости'),array('sort'=>'popular','news_period'=>'2','extra_class_name'=>'td-news-list','posts_per_page'=>15));?>
+    <?php $terekhin_framework->show_news_in_section_by_category($obj,($sub_title?$sub_title.' - ':'')."События",'events',array('display_read_more_button'=>'yes','extra_class_name'=>'td-news-list','posts_per_page'=>10));?>
+    <?php $terekhin_framework->show_news_in_section_by_category($obj, ($sub_title?$sub_title.' - ':'')."Мнения", 'opinions',array('display_read_more_button'=>'yes','extra_class_name'=>'td-news-list','posts_per_page'=>10));
 
     ?>
-    <?php $terekhin_framework->show_news_in_section_by_category($obj,($sub_title?$sub_title.' - ':'')."Видеo",'videos',array('layout_view'=>'layout1','image_size'=>'large','display_read_more_button'=>'yes','column_number'=>2));?>
+    <?php $terekhin_framework->show_news_in_section_by_category($obj,($sub_title?$sub_title.' - ':'')."Видеo",'videos',array('layout_view'=>'layout1','image_size'=>'large','display_read_more_button'=>'yes','column_number'=>2,'posts_per_page'=>10));?>
 
 </div>
 <?php } ?>
