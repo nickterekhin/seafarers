67efc29617821df550201e56daa01d5303de290a
Nick Terekhin <nick.terekhin@gmail.com>
2018-03-26 01:41:05 +0300

    created complex short code for the main page

--- inc/config.php
+++ inc/config.php
@@ -39,7 +39,15 @@ function terekhin_dev_remove_elements()
     }
 
 }
+add_action( 'admin_bar_menu', 'remove_wp_nodes', 999 );
 
+function remove_wp_nodes()
+{
+    global $wp_admin_bar;
+    $wp_admin_bar->remove_node( 'new-post' );
+    $wp_admin_bar->remove_node( 'new-link' );
+    $wp_admin_bar->remove_node( 'new-media' );
+}
 function terekhin_dev_category_template($single)
 {
     global $post,$wp_query;
